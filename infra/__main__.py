e646fca8333957abbf0246e2dbc994aaa2224839
Tyler Hillery <tyhillery@gmail.com>
2024-11-02 17:27:49 -0700

    feat: provision digitalocean droplet (#6)
    * wip: provision digital ocean droplet
    
    * wip: provision digital ocean droplet
    
    * chore: update github actions
    
    * feat: update pulumi gha to include details
    
    * fix: update github action permission
    
    * chore: roll github token
    
    * fix: update pulumi gha to give it comment perms
    
    * fix: change concurrency and remote permissions
    
    * chore: update pulumi gha settings

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,7 +1,8 @@
 import pulumi
+import pulumi_digitalocean as digitalocean
 import pulumi_docker as docker
 from components import DockerImageComponent
-from utils import create_docker_resource
+from utils import create_docker_resource, get_cloud_init_script
 
 config = pulumi.Config()
 BACKEND_PATH = config.get("backend-path") or "../backend"
@@ -9,32 +10,58 @@ BACKEND_PORT = config.get_int("backend-port") or 8000
 BACKEND_SERVICE_NAME = config.get("backend-service-name") or "backend"
 CADDY_PATH = config.get("caddy-path") or "../caddy"
 CADDY_SERVICE_NAME = config.get("caddy-service-name") or "caddy"
+DIGITALOCEAN_SSH_KEY_ID = config.get_secret_int("digitalocean-ssh-key-id")
 DOMAIN = config.get("domain") or "localhost"
+GITHUB_USERNAME = config.get("github-username") or "tylerhillery"
+GITHUB_TOKEN = config.get_secret("github-token")
 PROTOCOL = config.get("protocol") or "http"
+PULUMI_ACCESS_TOKEN = config.get_secret("pulumi-access-token")
 STACK_NAME = pulumi.get_stack()
-
-pulumi.export("stack-name", STACK_NAME)
+TAILSCALE_AUTH_KEY = config.get_secret("tailscale-auth-key")
 
 caddy_image = DockerImageComponent(
     stack_name=STACK_NAME,
     service_name=CADDY_SERVICE_NAME,
     context=CADDY_PATH,
+    github_username=GITHUB_USERNAME,
+    github_token=GITHUB_TOKEN,
 )
 
-pulumi.export(f"{CADDY_SERVICE_NAME}-image-tag", caddy_image.image_tag)
 pulumi.export(f"{CADDY_SERVICE_NAME}-image-identifier", caddy_image.image_identifier)
 
 backend_image = DockerImageComponent(
     stack_name=STACK_NAME,
     service_name=BACKEND_SERVICE_NAME,
     context=BACKEND_PATH,
+    github_username=GITHUB_USERNAME,
+    github_token=GITHUB_TOKEN,
 )
 
-pulumi.export(f"{BACKEND_SERVICE_NAME}-image-tag", backend_image.image_tag)
 pulumi.export(
     f"{BACKEND_SERVICE_NAME}-image-identifier", backend_image.image_identifier
 )
 
+if STACK_NAME in "cicd":
+    user_data = get_cloud_init_script(
+        github_token=GITHUB_TOKEN,
+        pulumi_access_token=PULUMI_ACCESS_TOKEN,
+        tailscale_auth_key=TAILSCALE_AUTH_KEY,
+    )
+
+    droplet = digitalocean.Droplet(
+        "pypacktrends-droplet",
+        image="ubuntu-20-04-x64",
+        name="pypacktrends-prod",
+        region=digitalocean.Region.SFO3,
+        size=digitalocean.DropletSlug.DROPLET_S1_VCPU1_GB,
+        ssh_keys=[DIGITALOCEAN_SSH_KEY_ID],
+        tags=["pypacktrends", "prod"],
+        user_data=user_data,
+        opts=pulumi.ResourceOptions(
+            ignore_changes=["user_data"],
+        ),
+    )
+    pulumi.export("pypacktrends-ipv4", droplet.ipv4_address)
 
 if STACK_NAME in ["dev", "prod"]:
     caddy_network = create_docker_resource(docker.Network, CADDY_SERVICE_NAME)
@@ -61,6 +88,7 @@ if STACK_NAME in ["dev", "prod"]:
                 volume_name=caddy_config_volume.name, container_path="/config"
             ),
         ],
+        network_mode="bridge",
         networks_advanced=[
             docker.ContainerNetworksAdvancedArgs(name=caddy_network.name)
         ],
@@ -77,6 +105,7 @@ if STACK_NAME in ["dev", "prod"]:
         BACKEND_SERVICE_NAME,
         image=backend_image.image_identifier,
         ports=[docker.ContainerPortArgs(internal=BACKEND_PORT, external=BACKEND_PORT)],
+        network_mode="bridge",
         networks_advanced=[
             docker.ContainerNetworksAdvancedArgs(name=caddy_network.name)
         ],
--- infra/utils.py
+++ infra/utils.py
@@ -1,4 +1,5 @@
 from typing import Type, Any
+from jinja2 import Environment, FileSystemLoader
 import pulumi
 import pulumi_docker as docker
 
@@ -13,3 +14,12 @@ def create_docker_resource(
     resource = resource_type(resource_name, name=service_name, **kwargs)
     pulumi.export(resource_name, resource.name)
     return resource
+
+
+def get_cloud_init_script(**kwargs: dict[str, pulumi.Output]) -> Any:
+    def render_cloud_init_template(kwargs: dict[str, str]) -> Any:
+        env = Environment(loader=FileSystemLoader("./templates"))
+        template = env.get_template("cloud-init.yml")
+        return template.render(**kwargs)
+
+    return pulumi.Output.all(**kwargs).apply(render_cloud_init_template)
