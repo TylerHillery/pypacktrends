222f790bbec52b80cdbc734387f460efa8bb95c2
Tyler Hillery <tyhillery@gmail.com>
2024-11-26 17:25:12 -0700

    fix: normalize white space

--- backend/app/tests/alembic/test_utils.py
+++ backend/app/tests/alembic/test_utils.py
@@ -40,29 +40,42 @@ def test_read_sql_file_missing() -> None:
 
 
 def test_read_sql_file(alembic_sql_upgrade_file: Path) -> None:
-    result = read_sql_file(alembic_sql_upgrade_file)
-    expected_result = (
-        "pragma journal_mode = wal",
-        """create table pypi_package_downloads_per_day (
-    package_name            text    not null,
-    package_downloaded_date text    not null,
-    downloads               integer not null,
-    primary key (package_name, package_downloaded_date)
-) strict""",
-        """create index idx_package_name
-on pypi_package_downloads_per_day (package_name)""",
-        """create index idx_package_downloaded_date
-on pypi_package_downloads_per_day (package_downloaded_date)""",
-        """create table pypi_packages (
-    package_name            text not null primary key,
-    latest_package_version  text not null,
-    package_summary         text,
-    package_home_page       text,
-    package_download_url    text,
-    package_uploaded_at     text not null
-) strict""",
-        """create index idx_package_uploaded_at
-on pypi_packages (package_uploaded_at)""",
+    def normalize_whitespace(sql: str) -> str:
+        return " ".join(sql.split())
+
+    result = tuple(
+        normalize_whitespace(stmt) for stmt in read_sql_file(alembic_sql_upgrade_file)
+    )
+    expected_result = tuple(
+        normalize_whitespace(stmt)
+        for stmt in (
+            "pragma journal_mode = wal",
+            """create table pypi_package_downloads_per_day (
+                package_name            text    not null,
+                package_downloaded_date text    not null,
+                downloads               integer not null,
+                primary key (package_name, package_downloaded_date)
+            ) strict
+            """,
+            """create index idx_package_name
+               on pypi_package_downloads_per_day (package_name)
+            """,
+            """create index idx_package_downloaded_date
+               on pypi_package_downloads_per_day (package_downloaded_date)
+            """,
+            """create table pypi_packages (
+                package_name            text not null primary key,
+                latest_package_version  text not null,
+                package_summary         text,
+                package_home_page       text,
+                package_download_url    text,
+                package_uploaded_at     text not null
+            ) strict
+            """,
+            """create index idx_package_uploaded_at
+               on pypi_packages (package_uploaded_at)
+            """,
+        )
     )
     assert result == expected_result
 
