4ac47dbcdc8744fd96ae538ae911d55baca4681c
Tyler Hillery <tyhillery@gmail.com>
2024-12-07 08:13:15 -0700

    fix: dbt weeks since first distribution negative (#73)

--- dbt/models/marts/pypi/pypi_package_downloads_weekly_metrics.sql
+++ dbt/models/marts/pypi/pypi_package_downloads_weekly_metrics.sql
@@ -9,7 +9,10 @@ pypi_package_downloads_per_week as (
             partition by package_name
             order by package_downloaded_week
         )                                    as cumulative_downloads,
-        min(package_downloaded_week) over () as first_pypi_downloaded_week
+        min(package_downloaded_week) over () as first_pypi_downloaded_week,
+        min(package_downloaded_week) over (
+            partition by package_name
+        )                                    as first_package_downloaded_week
     from
         {{ ref('pypi_package_downloads_per_week') }}
 ),
@@ -30,19 +33,26 @@ weeks_since_first_distribution as (
         downloads.cumulative_downloads,
         -- pypi download stats only go back so far so if the first distribution
         -- is before the min week from all packages in PyPI we set it to that first week
-        greatest(
-            packages.first_distribution_week,
-            downloads.first_pypi_downloaded_week
-        )     as first_distribution_week,
-        date_diff(
-            downloads.package_downloaded_week,
+        -- but sometimes the downloads is there before the min distribution week
+        -- which theoritcally should not be possible because how can you download
+        -- something before the first distribution but nonethless we need to take the
+        -- least of the two
+        least(
             greatest(packages.first_distribution_week, downloads.first_pypi_downloaded_week),
-            week
-        ) + 1 as weeks_since_first_distribution
+            downloads.first_package_downloaded_week
+        ) as first_distribution_week
     from
         pypi_package_downloads_per_week as downloads
     inner join pypi_package_first_distribution_week as packages
         on downloads.package_name = packages.package_name
 )
 
-select * from weeks_since_first_distribution
+select
+    *,
+    date_diff(
+        package_downloaded_week,
+        first_distribution_week,
+        week
+    ) + 1 as weeks_since_first_distribution
+from
+    weeks_since_first_distribution
