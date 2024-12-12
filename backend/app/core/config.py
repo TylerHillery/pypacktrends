ecb5f87440acde9f5f8001dbbb8dd881653cacce
Tyler Hillery <tyhillery@gmail.com>
2024-12-12 06:27:17 -0700

    feat: reduce sync memory usage (#92)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 from typing import Literal
 
-from fastapi.templating import Jinja2Templates
 from pydantic import HttpUrl, computed_field, field_validator
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "pypacktrends"
-    TEMPLATES: Jinja2Templates = Jinja2Templates(directory="app/templates")
     ENVIRONMENT: Literal["dev", "ci", "prod"] = "dev"
     SENTRY_DSN: HttpUrl | None = None
     SQLITE_DB_PATH: Path = (
@@ -35,4 +33,3 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
-templates = settings.TEMPLATES
--- backend/app/views/main.py
+++ backend/app/views/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.views.routes import home, packages, search
 
-api_router = APIRouter()
+api_router: APIRouter = APIRouter()
 api_router.include_router(home.router, tags=["home"])
 api_router.include_router(search.router, tags=["search"])
 api_router.include_router(packages.router, tags=["packages"])
